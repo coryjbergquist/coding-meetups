f4b886b4cc15a13fe369ca390d384dce8a7270ea
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-06-23 16:21:51 +0000

    Done.

--- coding_meetups/lib/coding_meetups.rb
+++ coding_meetups/lib/coding_meetups.rb
@@ -1,7 +1,4 @@
 require_relative "./coding_meetups/version"
 
-module CodingMeetups
-  # Your code goes here...
-end
 
 require_relative "./coding_meetups/cli"
--- coding_meetups/lib/coding_meetups/cli.rb
+++ coding_meetups/lib/coding_meetups/cli.rb
@@ -4,5 +4,12 @@ class CodingMeetups::CLI
 
   def call
     puts "these are the meetups"
+    list_meetups
   end
+
+  def list_meetups
+    puts "Phoenix meetup 1"
+    puts "Phoenix meetup 2"
+  end
+
 end
