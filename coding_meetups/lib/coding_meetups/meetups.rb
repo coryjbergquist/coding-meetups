bd2b59321f83288e5b62532efe1df997137f089a
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-06-24 19:32:09 +0000

    removed unnecessary comments and prys

--- coding_meetups/lib/coding_meetups/meetups.rb
+++ coding_meetups/lib/coding_meetups/meetups.rb
@@ -7,7 +7,6 @@ class CodingMeetups::Meetups
     @@meetup_list = []
 
   def self.all
-    #returns a bunch of instances of coding meetups
     #scrape coding website, then return meetups
     @@meetup_list = []
     websites
@@ -37,69 +36,5 @@ class CodingMeetups::Meetups
       @@meetup_list << meetup_1
     end
   end
-
-
-  # def self.scrape_group_1
-  #
-  #   doc = Nokogiri::HTML(open("https://www.meetup.com/webdesignersdevelopers/"))
-  #   meetup_1 = self.new
-  #   meetup_1.name = doc.css(".groupHomeHeader-groupNameLink").text
-  #   meetup_1.time = "Next meeting is #{doc.css(".eventCard--clickable .eventTimeDisplay").first.text}"
-  #   meetup_1.info = doc.css(".group-home .group-description").text
-  #   meetup_1
-  #
-  # end
-  #
-  # def self.scrape_group_2
-  #
-  #   doc = Nokogiri::HTML(open("https://www.meetup.com/Thinkful-Phoenix/"))
-  #   meetup_2 = self.new
-  #   meetup_2.name = doc.css(".groupHomeHeader-groupNameLink").text
-  #   meetup_2.time = "Next meeting is #{doc.css(".eventCard--clickable .eventTimeDisplay").first.text}"
-  #   meetup_2.info = doc.css(".group-home .group-description").text
-  #   meetup_2
-  #
-  # end
-  #
-  # def self.scrape_group_3
-  #
-  #   doc = Nokogiri::HTML(open("https://www.meetup.com/Phoenix-Python-Meetup-Group/"))
-  #   meetup_3 = self.new
-  #   meetup_3.name = doc.css(".groupHomeHeader-groupNameLink").text
-  #   meetup_3.time = "Next meeting is #{doc.css(".eventCard--clickable .eventTimeDisplay").first.text}"
-  #   meetup_3.info = doc.css(".group-home .group-description").text
-  #   meetup_3
-  #
-  # end
-  #
-  # def self.scrape_group_4
-  #
-  #   doc = Nokogiri::HTML(open("https://www.meetup.com/azPHPUG/"))
-  #   meetup_4 = self.new
-  #   meetup_4.name = doc.css(".groupHomeHeader-groupNameLink").text
-  #   meetup_4.time = "Next meeting is #{doc.css(".eventCard--clickable .eventTimeDisplay").first.text}"
-  #   meetup_4.info = doc.css(".group-home .group-description").text
-  #   meetup_4
-  #
-  # end
-  #
-  # def self.scrape_group_5
-  #
-  #   doc = Nokogiri::HTML(open("https://www.meetup.com/Phoenix-JavaScript/"))
-  #   meetup_5 = self.new
-  #   meetup_5.name = doc.css(".groupHomeHeader-groupNameLink").text
-  #   meetup_5.time = "Next meeting is #{doc.css(".eventCard--clickable .eventTimeDisplay").first.text}"
-  #   meetup_5.info = doc.css(".group-home .group-description").text
-  #   meetup_5
-  #
-  # end
-
-
-
-
-
-#  searching for this link within the current page "https://www.meetup.com/webdesignersdevelopers/events/mlhpxnyxkbvb/"
-#  doc.css("div#outlet #root .flex .flex-item #mupMain .bounds .child-wrapper .chunk .eventList")
-
 
 end
